e5fe61a7c729d073b512c8afc9e886db13aca63e
Tony Muhammad Yousefnezhad <myousefnezhad@gmail.com>
2020-04-18 21:32:48 -0600

    V1.8B8500

--- Base/AR.py
+++ Base/AR.py
@@ -1,11 +1,9 @@
 import numpy as np
 
-
-
-def AR(T, rho, rank = 1):
+def AR(T, rho, rank = 1, inv=False):
     P = np.zeros((T, T))
     for i in range(T):
         for j in range(i - rank, i):
             if i >= 0 and j >= 0:
                 P[i, j] = 1
-    return np.linalg.inv(np.eye(T) - rho * P)
+    return np.linalg.inv(np.eye(T) - rho * P) if inv else np.eye(T) - rho * P
