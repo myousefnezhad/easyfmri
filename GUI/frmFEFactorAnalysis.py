923980850ea92a061bd871b0bebc063670e9bfca
Tony Muhammad Yousefnezhad <myousefnezhad@gmail.com>
2020-07-15 12:18:39 -0600

    V1.8B8800

--- GUI/frmFEFactorAnalysis.py
+++ GUI/frmFEFactorAnalysis.py
@@ -20,15 +20,14 @@
 
 import os
 import sys
-
 import numpy as np
-import scipy.io as io
 from PyQt5.QtWidgets import *
 from sklearn import preprocessing
-from sklearn.decomposition import FactorAnalysis
+from GUI.frmFEFactorAnalysisGUI import *
 from Base.dialogs import LoadFile, SaveFile
 from Base.utility import getVersion, getBuild
-from GUI.frmFEFactorAnalysisGUI import *
+from sklearn.decomposition import FactorAnalysis
+from IO.mainIO import mainIO_load, mainIO_save, reshape_1Dvector
 
 
 class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
@@ -73,12 +72,12 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
 
 
     def btnInFile_click(self):
-        filename = LoadFile("Load MatLab data file ...",['MatLab files (*.mat)'],'mat',\
+        filename = LoadFile("Load data file ...",['Data files (*.ezx *.mat *.ezdata)'],'ezx',\
                             os.path.dirname(ui.txtInFile.text()))
         if len(filename):
             if os.path.isfile(filename):
                 try:
-                    data = io.loadmat(filename)
+                    data = mainIO_load(filename)
                     Keys = data.keys()
 
                     # Data
@@ -201,14 +200,11 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
                     ui.cbNScan.setChecked(HasDefualt)
 
                     # set number of features
-                    data = io.loadmat(filename)
                     XShape = np.shape(data[ui.txtData.currentText()])
                     ui.txtNumFea.setMaximum(1)
                     ui.txtNumFea.setMaximum(XShape[1])
                     ui.txtNumFea.setValue(XShape[1])
                     ui.lblFeaNum.setText("1 ... " + str(XShape[1]))
-
-
 
                     ui.txtInFile.setText(filename)
                 except Exception as e:
@@ -219,16 +215,14 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
                 print("File not found!")
 
     def btnOutFile_click(self):
-        ofile = SaveFile("Save MatLab data file ...",['MatLab files (*.mat)'],'mat',\
+        ofile = SaveFile("Save data file ...",['Data files (*.ezx *.mat)'],'ezx',\
                              os.path.dirname(ui.txtOutFile.text()))
         if len(ofile):
             ui.txtOutFile.setText(ofile)
 
     def btnConvert_click(self):
         msgBox = QMessageBox()
-
         SVD = ui.cbSVD.currentText()
-
         # Tol
         try:
             Tol = np.float(ui.txtTole.text())
@@ -238,7 +232,6 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
             msgBox.setStandardButtons(QMessageBox.Ok)
             msgBox.exec_()
             return False
-
         # MaxIte
         try:
             MaxIter = np.int32(ui.txtMaxIter.text())
@@ -248,14 +241,12 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
             msgBox.setStandardButtons(QMessageBox.Ok)
             msgBox.exec_()
             return False
-
         if MaxIter < 1:
             msgBox.setText("Maximum number of iterations is wrong!")
             msgBox.setIcon(QMessageBox.Critical)
             msgBox.setStandardButtons(QMessageBox.Ok)
             msgBox.exec_()
             return False
-
         # PIter
         try:
             PIter = np.int32(ui.txtPIter.text())
@@ -265,14 +256,12 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
             msgBox.setStandardButtons(QMessageBox.Ok)
             msgBox.exec_()
             return False
-
         if PIter < 1:
             msgBox.setText("Power iterations is wrong!")
             msgBox.setIcon(QMessageBox.Critical)
             msgBox.setStandardButtons(QMessageBox.Ok)
             msgBox.exec_()
             return False
-
         # OutFile
         OutFile = ui.txtOutFile.text()
         if not len(OutFile):
@@ -281,7 +270,6 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
             msgBox.setStandardButtons(QMessageBox.Ok)
             msgBox.exec_()
             return False
-
         # InFile
         InFile = ui.txtInFile.text()
         if not len(InFile):
@@ -290,42 +278,35 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
             msgBox.setStandardButtons(QMessageBox.Ok)
             msgBox.exec_()
             return False
-
         if not os.path.isfile(InFile):
             msgBox.setText("Input file not found!")
             msgBox.setIcon(QMessageBox.Critical)
             msgBox.setStandardButtons(QMessageBox.Ok)
             msgBox.exec_()
             return False
-
         if ui.rbScale.isChecked() == True and ui.rbALScale.isChecked() == False:
             msgBox.setText("Subject Level Normalization is just available for Subject Level Analysis!")
             msgBox.setIcon(QMessageBox.Critical)
             msgBox.setStandardButtons(QMessageBox.Ok)
             msgBox.exec_()
             return False
-
-        InData = io.loadmat(InFile)
+        InData = mainIO_load(InFile)
         OutData = dict()
-        OutData["imgShape"] = InData["imgShape"]
-
+        OutData["imgShape"] = reshape_1Dvector(InData["imgShape"])
         if not len(ui.txtData.currentText()):
             msgBox.setText("Please enter Data variable name!")
             msgBox.setIcon(QMessageBox.Critical)
             msgBox.setStandardButtons(QMessageBox.Ok)
             msgBox.exec_()
             return False
-
         try:
             X = InData[ui.txtData.currentText()]
-
             if ui.cbScale.isChecked() and (not ui.rbScale.isChecked()):
                 X = preprocessing.scale(X)
                 print("Whole of data is scaled X~N(0,1).")
         except:
             print("Cannot load data")
             return
-
         try:
             NumFea = np.int32(ui.txtNumFea.text())
         except:
@@ -340,14 +321,12 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
             msgBox.setStandardButtons(QMessageBox.Ok)
             msgBox.exec_()
             return False
-
         if NumFea > np.shape(X)[1]:
             msgBox.setText("Number of features is wrong!")
             msgBox.setIcon(QMessageBox.Critical)
             msgBox.setStandardButtons(QMessageBox.Ok)
             msgBox.exec_()
             return False
-
         # Subject
         if not len(ui.txtSubject.currentText()):
             msgBox.setText("Please enter Subject variable name!")
@@ -355,14 +334,12 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
             msgBox.setStandardButtons(QMessageBox.Ok)
             msgBox.exec_()
             return False
-
         try:
             Subject = InData[ui.txtSubject.currentText()]
-            OutData[ui.txtOSubject.text()] = Subject
+            OutData[ui.txtOSubject.text()] = reshape_1Dvector(Subject)
         except:
             print("Cannot load Subject ID")
             return
-
         # Label
         if not len(ui.txtLabel.currentText()):
                 msgBox.setText("Please enter Label variable name!")
@@ -370,9 +347,7 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
                 msgBox.setStandardButtons(QMessageBox.Ok)
                 msgBox.exec_()
                 return False
-        OutData[ui.txtOLabel.text()] = InData[ui.txtLabel.currentText()]
-
-
+        OutData[ui.txtOLabel.text()] = reshape_1Dvector(InData[ui.txtLabel.currentText()])
         # Task
         if ui.cbTask.isChecked():
             if not len(ui.txtTask.currentText()):
@@ -381,8 +356,7 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
                 msgBox.setStandardButtons(QMessageBox.Ok)
                 msgBox.exec_()
                 return False
-            OutData[ui.txtOTask.text()] = InData[ui.txtTask.currentText()]
-
+            OutData[ui.txtOTask.text()] = reshape_1Dvector(InData[ui.txtTask.currentText()])
         # Run
         if ui.cbRun.isChecked():
             if not len(ui.txtRun.currentText()):
@@ -391,9 +365,7 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
                 msgBox.setStandardButtons(QMessageBox.Ok)
                 msgBox.exec_()
                 return False
-            OutData[ui.txtORun.text()] = InData[ui.txtRun.currentText()]
-
-
+            OutData[ui.txtORun.text()] = reshape_1Dvector(InData[ui.txtRun.currentText()])
         # Counter
         if ui.cbCounter.isChecked():
             if not len(ui.txtCounter.currentText()):
@@ -402,8 +374,7 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
                 msgBox.setStandardButtons(QMessageBox.Ok)
                 msgBox.exec_()
                 return False
-            OutData[ui.txtOCounter.text()] = InData[ui.txtCounter.currentText()]
-
+            OutData[ui.txtOCounter.text()] = reshape_1Dvector(InData[ui.txtCounter.currentText()])
         # Matrix Label
         if ui.cbmLabel.isChecked():
             if not len(ui.txtmLabel.currentText()):
@@ -413,7 +384,6 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
                 msgBox.exec_()
                 return False
             OutData[ui.txtOmLabel.text()] = InData[ui.txtmLabel.currentText()]
-
         # Design
         if ui.cbDM.isChecked():
             if not len(ui.txtDM.currentText()):
@@ -423,7 +393,6 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
                 msgBox.exec_()
                 return False
             OutData[ui.txtODM.text()] = InData[ui.txtDM.currentText()]
-
         # Coordinate
         if ui.cbCol.isChecked():
             if not len(ui.txtCol.currentText()):
@@ -433,7 +402,6 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
                 msgBox.exec_()
                 return False
             OutData[ui.txtOCol.text()] = InData[ui.txtCol.currentText()]
-
         # Condition
         if ui.cbCond.isChecked():
             if not len(ui.txtCond.currentText()):
@@ -443,7 +411,6 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
                 msgBox.exec_()
                 return False
             OutData[ui.txtOCond.text()] = InData[ui.txtCond.currentText()]
-
         # Number of Scan
         if ui.cbNScan.isChecked():
             if not len(ui.txtScan.currentText()):
@@ -452,11 +419,9 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
                 msgBox.setStandardButtons(QMessageBox.Ok)
                 msgBox.exec_()
                 return False
-            OutData[ui.txtOScan.text()] = InData[ui.txtScan.currentText()]
-
+            OutData[ui.txtOScan.text()] = reshape_1Dvector(InData[ui.txtScan.currentText()])
         Models = dict()
         Models["Name"] = "FactorAnalysis"
-
         if ui.rbALScale.isChecked():
             print("Partition data to subject level ...")
             SubjectUniq = np.unique(Subject)
@@ -468,17 +433,14 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
                 else:
                     X_Sub.append(X[np.where(Subject == subj)[1],:])
                 print("Subject ", subj, " is extracted from data.")
-
             print("Running Factor Analysis in subject level ...")
             X_Sub_PCA = list()
             lenPCA    = len(X_Sub)
-
             for xsubindx, xsub in enumerate(X_Sub):
                 model = FactorAnalysis(n_components=NumFea,tol=Tol,max_iter=MaxIter,svd_method=SVD,iterated_power=PIter)
                 X_Sub_PCA.append(model.fit_transform(xsub))
                 Models["Model" + str(xsubindx + 1)] = str(model.get_params(deep=True))
                 print("Factor Analysis: ", xsubindx + 1, " of ", lenPCA, " is done.")
-
             print("Data integration ... ")
             X_new = None
             for xsubindx, xsub in enumerate(X_Sub_PCA):
@@ -490,11 +452,9 @@ class frmFEFactorAnalysis(Ui_frmFEFactorAnalysis):
             model = FactorAnalysis(n_components=NumFea, tol=Tol, max_iter=MaxIter, svd_method=SVD, iterated_power=PIter)
             OutData[ui.txtOData.text()] = model.fit_transform(X)
             Models["Model"] = str(model.get_params(deep=True))
-
         OutData["ModelParameter"] = Models
-
         print("Saving ...")
-        io.savemat(ui.txtOutFile.text(), mdict=OutData)
+        mainIO_save(OutData, ui.txtOutFile.text())
         print("DONE.")
         msgBox.setText("Factor Analysis is done.")
         msgBox.setIcon(QMessageBox.Information)
